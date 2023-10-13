e7149b8b45dc702d9456ae4cca7af7b2c36613f3
Boriswp <boriswp99@gmail.com>
2023-10-13 23:42:25 +0500

    add typewriter

--- Mobile/app/src/main/java/com/tzeentch/workfinder/ui/composables/Greeting.kt
+++ Mobile/app/src/main/java/com/tzeentch/workfinder/ui/composables/Greeting.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,19 +14,22 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.tzeentch.workfinder.NavigationItem
+import com.tzeentch.workfinder.ui.composables.components.TypewriterText
 import com.tzeentch.workfinder.viewModels.MainViewModel
 
 @Composable
-fun Greeting(navController: NavController,viewModel: MainViewModel) {
+fun Greeting(navController: NavController, viewModel: MainViewModel) {
     Column(
-        modifier = Modifier.padding(top = 45.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+
+        TypewriterText(texts = listOf("Hello!", "Salom!", "Привет!"))
+
+        Spacer(modifier = Modifier.height(45.dp))
+
         Text(
-            text = "Привет!\n" +
-                    "Я твой интеллектуальный помошник.\n" +
-                    "Помогу тебе с поиском работы.",
+            text = "I am your intellectual assistant.\n" + "I will help you find a job.",
             modifier = Modifier.fillMaxWidth(),
             textAlign = TextAlign.Center
         )
@@ -35,7 +37,7 @@ fun Greeting(navController: NavController,viewModel: MainViewModel) {
         Spacer(modifier = Modifier.height(45.dp))
 
         Button(onClick = { navController.navigate(NavigationItem.Authorization.route) }) {
-            Text(text = "Приступим")
+            Text(text = "Continue")
         }
     }
 }
---
+++ Mobile/app/src/main/java/com/tzeentch/workfinder/ui/composables/components/TypeWriter.kt
@@ -0,0 +1,52 @@
+package com.tzeentch.workfinder.ui.composables.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+
+@Composable
+fun TypewriterText(
+    texts: List<String>,
+) {
+    var textIndex by remember {
+        mutableIntStateOf(0)
+    }
+    var textToDisplay by remember {
+        mutableStateOf("")
+    }
+
+    LaunchedEffect(
+        key1 = texts,
+    ) {
+        while (textIndex < texts.size) {
+            texts[textIndex].forEachIndexed { charIndex, _ ->
+                textToDisplay = texts[textIndex]
+                    .substring(
+                        startIndex = 0,
+                        endIndex = charIndex + 1,
+                    )
+                delay(160)
+            }
+            textIndex = (textIndex + 1) % texts.size
+            delay(1000)
+        }
+    }
+
+    Text(
+        text = textToDisplay,
+        modifier = Modifier.textBrush(Brush.horizontalGradient(listOf(Color.Magenta, Color.Cyan))),
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold,
+    )
+}
