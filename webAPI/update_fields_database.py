89989c5079236703777b8b1ee940d0cd49c5c462
DostavalovID <igor.d@ptolemay.com>
2023-10-15 00:20:22 +0500

    hh recommendations fixed

--- webAPI/update_fields_database.py
+++ webAPI/update_fields_database.py
@@ -1,12 +1,13 @@
 from bert_utils import SentenceTransformer
 from hh_parser import HHAPI
 import json
-area_id = 97 # Uzbekistan
+
+area_id = 97  # Uzbekistan
 api = HHAPI(area_id)
 init_model = SentenceTransformer()
 
 
-def preprocess(work_fields):
+def preprocess_work_fields(work_fields):
     field_names = list(work_fields.keys())
 
     embeddings = init_model(field_names)
@@ -16,41 +17,67 @@ def preprocess(work_fields):
         processed_data.append({
             "name": name,
             "count": work_fields[name],
-            "embedding": embeddings[idx].tolist()  # Convert tensor to list
+            "embedding": embeddings[idx].tolist()
         })
 
     return processed_data
+
+def preprocess_vacancies(vacancies):
+    names = [vacancy['name'] for vacancy in vacancies]
+    specializations = [vacancy['specialization'] for vacancy in vacancies]
+    employments = [vacancy['employment'] for vacancy in vacancies]
+
+    names_embeddings = init_model(names)
+    specializations_embeddings = init_model(specializations)
+    employments_embeddings = init_model(employments)
+
+    processed_vacancies = []
+
+    for idx, vacancy in enumerate(vacancies):
+        processed_vacancies.append({
+            "url": vacancy["url"],
+            "name": vacancy["name"],
+            "name_embedding": names_embeddings[idx].tolist(),
+            "specialization": vacancy["specialization"],
+            "specialization_embedding": specializations_embeddings[idx].tolist(),
+            "employment": vacancy["employment"],
+            "employment_embedding": employments_embeddings[idx].tolist(),
+        })
+
+    return processed_vacancies
+
+
+
+
 
 def save_to_json(data, filename):
     with open(filename, 'w', encoding='utf-8') as outfile:
         json.dump(data, outfile, ensure_ascii=False)
 
+
 def load_from_json(filename):
     with open(filename, 'r', encoding='utf-8') as infile:
         data = json.load(infile)
-
-    # Convert the embedding lists back to PyTorch tensors
-    # for item in data:
-    #     item['embedding'] = torch.tensor(item['embedding'])
-
     return data
 
 
 def update():
     api.fetch_specializations()
-    fields_count = api.fetch_vacancies()
-    processed_data = preprocess(work_fields=fields_count)
-    return processed_data
+    fields_count, cleaned_vacancies = api.fetch_vacancies()
+    processed_fields = preprocess_work_fields(work_fields=fields_count)
+    processed_vacancies = preprocess_vacancies(vacancies=cleaned_vacancies)
+    return processed_fields, processed_vacancies
 
 
 if __name__ == "__main__":
     import time
 
     start = time.time()
-    processed_data = update()
+    processed_data, processed_vacancies = update()
     print('processed_data', processed_data)
     end = time.time()
 
     print('time taken', end - start)
 
-    save_to_json(processed_data, 'processed_data.json')
+    save_to_json(processed_data, 'processed_fields.json')
+    save_to_json(processed_vacancies, 'processed_vacancies.json')
